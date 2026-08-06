520ee9f7d5af980ddddfa36f7067c7d858ff0a76
Hrafn <5185245+hrafn@users.noreply.github.com>
2026-08-06 15:36:29 +0000

    Add missing imports

--- .teamcity/Windows/Project.kt
+++ .teamcity/Windows/Project.kt
@@ -13,6 +13,7 @@ import jetbrains.buildServer.configs.kotlin.buildFeatures.nuGetFeedCredentials
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.buildFeatures.sshAgent
+import jetbrains.buildServer.configs.kotlin.buildFeatures.untrustedBuildsSettings
 import jetbrains.buildServer.configs.kotlin.buildFeatures.xmlReport
 import jetbrains.buildServer.configs.kotlin.buildSteps.exec
 import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
