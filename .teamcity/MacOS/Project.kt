8afa0b2dd91713539e0eed80c00266fab78f466b
YummierGravy <cian@triarii.dev>
2026-08-07 16:55:16 +0200

    Sync PR triggering rules to template

--- .teamcity/MacOS/Project.kt
+++ .teamcity/MacOS/Project.kt
@@ -154,7 +154,7 @@ class CarbonBuildMacOS(buildName: String, configType: String, preset: String, ag
                 authType = token {
                     token = "%GITHUB_CARBON_PAT%"
                 }
-                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+                filterAuthorRole = PullRequests.GitHubRoleFilter.EVERYBODY
             }
         }
         commitStatusPublisher {
